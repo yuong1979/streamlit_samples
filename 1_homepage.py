4114ad5985e8e823dd395993de14fccc9ab79eea
yuong1979 <yuong1979@gmail.com>
2022-07-27 19:29:29 +0800

    added multipage tutorial

---
+++ 1_homepage.py
@@ -0,0 +1,22 @@
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Multipage App",
+    page_icon="👋",
+)
+
+st.title("Main Page")
+st.sidebar.success("Select a page above.")
+
+if "my_input" not in st.session_state:
+    st.session_state["my_input"] = ""
+
+my_input = st.text_input("Input a text here", st.session_state["my_input"])
+submit = st.button("Submit")
+if submit:
+    st.session_state["my_input"] = my_input
+    st.write("You have entered: ", my_input)
+
+
+# based on tutorial - https://youtu.be/YClmpnpszq8?list=PL7QI8ORyVSCaejt2LICRQtOTwmPiwKO2n
